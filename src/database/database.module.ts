394335e23d8bee1360ba2559695c76d13b9dfe59
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-11-26 08:21:15 -0300

    feat: add configuration for database connection

---
+++ src/database/database.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {  DataSourceOptions } from 'typeorm';
+
+export const dataSourseOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'devtraining',
+  entities: [],
+  synchronize: true, 
+
+}
+
+@Module({
+  imports: [
+  TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        return {
+          ...dataSourseOptions
+        }
+      }
+    })
+      
+  ]
+})
+export class DatabaseModule {}
+
+
+// synchronize: true, enquanto nao temos as migracoes, ele ira subir tabela automaticamente no db de acordo com nossas entidade decoradas
