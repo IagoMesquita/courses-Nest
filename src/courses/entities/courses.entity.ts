3d3822b8d577b6772b0807d3256a1d58428ac2b3
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-11-26 21:42:48 -0300

    feat: add many to many relationship between course and tag

--- src/courses/entities/courses.entity.ts
+++ src/courses/entities/courses.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Tag } from "./tags.entity";
+import { RelationCountAttribute } from "typeorm/query-builder/relation-count/RelationCountAttribute";
 
 @Entity('courses')
 export class Course {
@@ -11,8 +13,12 @@ export class Course {
   @Column()
   description: string;
   
-  @Column('json', { nullable: true})
-  tags: string[];
+  @JoinTable()
+  @ManyToMany(() => Tag, (tag) => tag.courses)
+  tags: Tag[];
 }
 
-// @Column('json', { nullable: true}) -> tags sera um json no banco de dados, e pode ser nulo 
+// ManyToMany: 
+// - primeiro parametro e o alvo ao qual entidade se relaciona
+// - segundo parametro e o iverso pega o elemento que deve estar na outra tabela
+// JoinTable e apenas para a tabela proprietaria, no nesse caso e Course, course possiu as tags
