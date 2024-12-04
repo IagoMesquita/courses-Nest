0b5c2315f8fddec2f9444440b7f0083211e939ea
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-12-04 08:35:06 -0300

    test: app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Vamo que vamo!!';
+    return 'Bem vindos, aplicacao no ar!';
   }
 }
